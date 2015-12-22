f54cce308edc08b274237f75ed8b4db480e3db63
Tofique Sikder <emran.sikder@gmail.com>
2015-12-21 22:39:41 -0500

    soap

---
+++ src/main/java/com/concretepage/StudentEndpoint.java
@@ -0,0 +1,27 @@
+package com.concretepage;
+
+import com.concretepage.soap.GetStudentRequest;
+import com.concretepage.soap.GetStudentResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+/**
+ * Created by adity on 12/21/2015.
+ */
+@Endpoint
+public class StudentEndpoint {
+    private static final String NAMESPACE_URI = "http://concretepage.com/soap";
+    @Autowired
+    private StudentUtility studentUtility;
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getStudentRequest")
+    @ResponsePayload
+    public GetStudentResponse getCountry(@RequestPayload GetStudentRequest request) {
+        GetStudentResponse response = new GetStudentResponse();
+        response.setStudent(studentUtility.getStudent(request.getStudentId()));
+        return response;
+    }
+}
---
+++ src/main/java/com/concretepage/WebAppInitializer.java
@@ -0,0 +1,30 @@
+package com.concretepage;
+
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration.Dynamic;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.ws.transport.http.MessageDispatcherServlet;
+
+/**
+ * Created by adity on 12/21/2015.
+ */
+
+public class WebAppInitializer implements WebApplicationInitializer {
+
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+        ctx.register(AppConfig.class);
+        ctx.setServletContext(servletContext);
+        MessageDispatcherServlet servlet = new MessageDispatcherServlet();
+        servlet.setApplicationContext(ctx);
+        servlet.setTransformWsdlLocations(true);
+        Dynamic dynamic = servletContext.addServlet("dispatcher",servlet);
+        dynamic.addMapping("/soapws*/ ");
+        dynamic.setLoadOnStartup(1);
+    }
+
+}
+
